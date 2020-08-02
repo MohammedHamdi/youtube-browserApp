d7bf5a472d64ac6d15e99729be38a4eca80e6019
Mohammed Hamdi <mhah000@hotmail.com>
2020-08-02 19:31:14 +0200

    Initial xcode project creation

---
+++ youtube-browserApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-browserApp
+//
+//  Created by Mohammed Hamdi on 8/2/20.
+//  Copyright © 2020 Mohammed Hamdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
