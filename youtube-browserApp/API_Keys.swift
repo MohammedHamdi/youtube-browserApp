9075907269f52fe4bbcda73941282cc6f024569d
Mohammed Hamdi <mhah000@hotmail.com>
2020-08-03 21:27:59 +0200

    Remove API Keys

---
+++ youtube-browserApp/API_Keys.swift
@@ -0,0 +1,11 @@
+//
+//  API_Keys.swift
+//  youtube-browserApp
+//
+//  Created by Mohammed Hamdi on 8/3/20.
+//  Copyright © 2020 Mohammed Hamdi. All rights reserved.
+//
+
+import Foundation
+
+let YOUTUBE_API_KEY = "AIzaSyAlxPNJb4Q55q20hSbOlja26dqv83UFCsE"
