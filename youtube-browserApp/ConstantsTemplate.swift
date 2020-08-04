43d4aff1a2833c52f6000ef17a417f3c72cfe7d2
Mohammed Hamdi <mhah000@hotmail.com>
2020-08-04 17:55:42 +0200

    Modified gitignore to stop tracking Constants.swift, added ConstantsTemplate.swift instead

--- youtube-browserApp/ConstantsTemplate.swift
+++ youtube-browserApp/ConstantsTemplate.swift
@@ -1,16 +1,20 @@
 //
-//  Constants.swift
+//  ConstantsTemplate.swift
 //  youtube-browserApp
 //
-//  Created by Mohammed Hamdi on 8/3/20.
+//  Created by Mohammed Hamdi on 8/4/20.
 //  Copyright © 2020 Mohammed Hamdi. All rights reserved.
 //
 
+// Add your own keys and uncomment the code
+
 import Foundation
 
+/*
 struct Constants {
     
-    static var API_KEY = ""//YOUTUBE_API_KEY
-    static var PLAYLIST_ID = "UUBJycsmduvYEL83R_U4JriQ"
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
 }
+*/
